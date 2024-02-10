19fa35a1085201c639e5bc2cd012d69212eb08dc
Artem Tolmachev <artem.tolmachev.work@gmail.com>
2024-02-10 19:54:47 +0300

    Add mobile main page

--- sphereapp/static/sphereapp/home_page/home_page.js
+++ sphereapp/static/sphereapp/home_page/home_page.js
@@ -10,3 +10,20 @@ const viewer = new PANOLENS.Viewer({
 
 viewer.add(homePagePhotosphere);
 viewer.OrbitControls.noZoom = true;
+
+const menuHamburger = document.querySelector(".menu-hamburger");
+const menuBar = document.querySelector(".menu-bar");
+const html = document.querySelector("html");
+const menuItem = document.querySelector(".menu-item")
+
+menuHamburger.addEventListener('click', () => {
+    menuHamburger.classList.toggle('active');
+    menuBar.classList.toggle('active');
+    html.classList.toggle('no-scroll');
+
+    menuItem.addEventListener('click', () => {
+        menuHamburger.classList.remove('active');
+        menuBar.classList.remove('active');
+        html.classList.remove('no-scroll');
+    });
+});
