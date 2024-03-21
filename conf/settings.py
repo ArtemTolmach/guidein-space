5dd95ee43c85644456e15d81a8eab044ea390e0b
Vlad Gerasov <v1lammer@gmail.com>
2024-03-22 01:34:42 +0300

    Add missed ALLOWED_HOSTS env variable

--- conf/settings.py
+++ conf/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 AUTH_USER_MODEL = 'guidein_space.User'
 LOGIN_REDIRECT_URL = '/'
