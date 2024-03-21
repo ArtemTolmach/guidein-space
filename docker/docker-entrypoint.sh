ddc8e7c28c05b32799dbc37686635a9fe9128370
Vlad Gerasov <v1lammer@gmail.com>
2024-03-22 01:07:47 +0300

    Fix docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,8 +6,10 @@ export ALLOWED_HOSTS=${ALLOWED_HOSTS:=127.0.0.1}
 
 export SECRET_KEY=${SECRET_KEY:=secret}
 
+export WORKERS_COUNT=${WORKERS_COUNT:=5}
+
 set +x
 
 python3 manage.py migrate
 
-gunicorn conf.wsgi
+gunicorn -b 0.0.0.0:8000 -w ${WORKERS_COUNT} conf.wsgi
