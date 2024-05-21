a26fc8b31fb5af1e097e3e525b439755b78a2d7a
V1ammer <v1lammer@gmail.com>
2024-05-21 15:51:09 +0300

    Change password hashing algorithm

--- conf/settings.py
+++ conf/settings.py
@@ -119,6 +119,12 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
